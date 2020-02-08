e087a045200a792afb5c7d923f351d59c717d834
BrGus1023 <brgus1023@gmail.com>
2020-02-08 14:09:24 -0600

    Subiendo Primera Aplicacion

---
+++ app.js
@@ -0,0 +1,6 @@
+const sobreMi={
+    primero:'Soy de Sistemas',
+    segundo:'Me Gusta programar'
+}
+document.getElementById('primero').innerHTML=sobreMi.primero
+document.getElementById('segundo').innerHTML=sobreMi.segundo
